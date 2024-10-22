9dcd62a16f0663e2a8dc4ea7eceaad2fe99951fd
kagamirudo <hoangvuph03@gmail.com>
2024-10-22 11:17:14 -0400

    update

--- Week 4/2bit_adder.py
+++ Week 4/2bit_adder.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 
 # Make the Adder into a 4 Wire Circuit
 adder = QuantumRegister(4)
-adder = QuantumCircuit(adder, name='adder')
+adder = QuantumCircuit(adder, name="adder")
 adder.barrier(range(4))
-adder.ccx(0,1,3)
-adder.cx(0,1)
-adder.ccx(1,2,3)
-adder.cx(1,2)
-adder.cx(0,1)
-adder.barrier(range(0,4))
+adder.ccx(0, 1, 3)
+adder.cx(0, 1)
+adder.ccx(1, 2, 3)
+adder.cx(1, 2)
+adder.cx(0, 1)
+adder.barrier(range(0, 4))
 myAdder = adder.to_instruction()
 
 in_bit = 7
@@ -29,10 +29,10 @@ qc.barrier(range(in_bit))
 for x in range(out_bit):
     qc.measure((in_bit - out_bit) + x, x)
 
-qc.draw(output="mpl",filename="circuit.png")
+qc.draw(output="mpl", filename="circuit.png")
 qc = qc.decompose()
 
-backend_sim = Aer.get_backend('qasm_simulator')
+backend_sim = Aer.get_backend("qasm_simulator")
 job_sim = backend_sim.run(qc, shots=test)
 # job_sim = execute(qc,backend_sim,shots=2048) # Cannot run
 
@@ -42,8 +42,6 @@ probs = {key: value / total_counts for key, value in counts.items()}
 fig = plot_histogram(probs, title="Quantum")
 fig.savefig("quan_result.png")
 
-truth_table = {'000': 1, '001': 2, '010': 3, '011': 4, 
-               '100': 3, '101': 2, '110': 1}
-fig = plot_histogram(truth_table, color='orange'
-                     , title="Truth Table")
+truth_table = {"000": 1, "001": 2, "010": 3, "011": 4, "100": 3, "101": 2, "110": 1}
+fig = plot_histogram(truth_table, color="orange", title="Truth Table")
 fig.savefig("actual_result.png")
